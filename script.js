446de60915e65ef57592fb375c8be0a97e865bff
Rajeev Roy <109815313+RajeevRoy120@users.noreply.github.com>
2025-03-18 20:48:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
+// Person class
 class Person {
-  private _name: string;
-  private _age: number;
-
-  constructor(name: string, age: number) {
+  constructor(name, age) {
     this._name = name;
     this._age = age;
   }
 
   // Getter for name
-  get name(): string {
+  get name() {
     return this._name;
   }
 
-  // Setter for name
-  set name(name: string) {
-    this._name = name;
+  // Setter for age
+  set age(age) {
+    this._age = age;
   }
 
   // Getter for age
-  get age(): number {
+  get age() {
     return this._age;
-  }
-
-  // Setter for age
-  set age(age: number) {
-    this._age = age;
   }
 }
 
-// Student class
+// Student class inheriting from Person
 class Student extends Person {
-  study(): void {
+  study() {
     console.log(`${this.name} is studying`);
   }
 }
 
-// Teacher class
+// Teacher class inheriting from Person
 class Teacher extends Person {
-  teach(): void {
+  teach() {
     console.log(`${this.name} is teaching`);
   }
 }
-
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+ 
 
